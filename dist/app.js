57c520966b3b28b6fa518b31a0bb9f2640030407
srinath-3898 <srinath3898@gmail.com>
2023-09-13 22:43:35 +0530

    Added dist and src folders

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const todosRoutes_1 = __importDefault(require("./routes/todosRoutes"));
@@ -12,5 +10,5 @@ const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
 app.use("/todos", todosRoutes_1.default);
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
